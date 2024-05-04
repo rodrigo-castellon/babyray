841e621268fc645ddbc0594591dd2691d13a53bb
Rodrigo Castellon <rjcaste@stanford.edu>
2024-05-03 21:21:49 -0700

    debug commit

--- go/cmd/worker/main.go
+++ go/cmd/worker/main.go
@@ -7,7 +7,7 @@ import (
     "strconv"
     "fmt"
     // "encoding/json"
-    "bytes"
+    // "bytes"
     "os/exec"
     "encoding/base64"
 
@@ -42,26 +42,31 @@ type server struct {
 
 func executeFunction(f []byte, args []byte, kwargs []byte) ([]byte, error) {
     // Prepare the command to run the Python script
-    cmd := exec.Command("python3", "execute.py")
+    // cmd := exec.Command("python3", "execute.py")
+    // cmd := exec.Command("which", "python3")
+    cmd := exec.Command("command", "-v", "python3", ">/dev/null", "&&", "echo", "$(which", "python3)", "||", "echo", `"not found"`)
 
     // Create a buffer to hold the serialized data
-    inputBuffer := bytes.NewBuffer(nil)
+    // inputBuffer := bytes.NewBuffer(nil)
 
-    // Write the function, args, and kwargs to the buffer
-    inputBuffer.Write(f)
-    inputBuffer.WriteByte('\n')
-    inputBuffer.Write(args)
-    inputBuffer.WriteByte('\n')
-    inputBuffer.Write(kwargs)
+    // // Write the function, args, and kwargs to the buffer
+    // inputBuffer.Write(f)
+    // inputBuffer.WriteByte('\n')
+    // inputBuffer.Write(args)
+    // inputBuffer.WriteByte('\n')
+    // inputBuffer.Write(kwargs)
 
-    // Set the stdin to our input buffer
-    cmd.Stdin = inputBuffer
+    // // Set the stdin to our input buffer
+    // cmd.Stdin = inputBuffer
 
     // Capture the output
     output, err := cmd.Output()
     if err != nil {
         log.Fatalf("Error executing function: %v", err)
     }
+
+    log.Printf("output: %v", output)
+    log.Printf("output: %s", output)
 
     // Decode the Base64 output to get the original pickled data
     data, err := base64.StdEncoding.DecodeString(string(output))
