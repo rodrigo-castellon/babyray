99d16f96772b9ad6695fc0c77ece9f85c3c81d84
Rodrigo Castellon <rjcaste@stanford.edu>
2024-05-03 21:51:07 -0700

    debug commit

--- go/cmd/worker/main.go
+++ go/cmd/worker/main.go
@@ -42,11 +42,11 @@ type server struct {
 
 func executeFunction(f []byte, args []byte, kwargs []byte) ([]byte, error) {
     // Prepare the command to run the Python script
-    // cmd := exec.Command("python3", "execute.py")
+    cmd := exec.Command("sh", "-c", "python3 execute.py")
     // cmd := exec.Command("which", "python3")
     // cmd := exec.Command("command", "-v", "python3", ">/dev/null", "&&", "echo", "$(which", "python3)", "||", "echo", `"not found"`)
     // cmd := exec.Command("which", "python3", ">/dev/null", "&&", "echo", "$(which", "python3)", "||", "echo", `"not found"`)
-    cmd := exec.Command("sh", "-c", "which python3")
+    // cmd := exec.Command("sh", "-c", "which python3")
 
     // Create a buffer to hold the serialized data
     // inputBuffer := bytes.NewBuffer(nil)
