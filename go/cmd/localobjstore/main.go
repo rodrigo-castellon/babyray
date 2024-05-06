6dea56bf2e10bc5a045935ba4a0e39aa832d1d88
trevleon <trev.leon7@gmail.com>
2024-05-05 17:38:17 -0700

    conn interface and cfg fix

--- go/cmd/localobjstore/main.go
+++ go/cmd/localobjstore/main.go
@@ -17,8 +17,9 @@ var localObjectStore map[uint32][]byte
 var localObjectChannels map[uint32]chan uint32
 var gcsObjClient GCSObjClient
 var localNodeID uint32
+var cfg Config
 func main() {
-    cfg := config.LoadConfig() // Load configuration
+    cfg = config.LoadConfig() // Load configuration
     address := ":" + strconv.Itoa(cfg.Ports.LocalObjectStore) // Prepare the network address
 
     lis, err := net.Listen("tcp", address)
@@ -60,15 +61,16 @@ func (s *server) Get(ctx context.Context, req *pb.GetRequest) (*pb.GetResponse, 
     nodeId := 1
     localObjectChannels[req.Uid] = make(chan uint32)
     gcsObjClient.RequestLocation(&pb.RequestLocationRequest{Uid: req.Uid, NodeId: nodeId})
-    localObjectStore[req.Uid] <- localObjectChannels[req.Uid]
+    localObjectStore[req.Uid] =  <- localObjectChannels[req.Uid]
     return &pb.GetResponse{Uid : req.Uid, ObjectBytes : localObjectStore[req.Uid]}, nil
 }
 
 func (s* server) LocationFound(ctx context.Context, resp *pb.LocationFoundResponse) (*pb.StatusResponse, error) {
     nodeID := resp.Location; 
-    otherLocalAddress := fmt.Sprintf("%s%d:%d", cfg.DNS.NodePrefix, nodeID, cfg.Ports.LocalScheduler)
+    otherLocalAddress := fmt.Sprintf("%s%d:%d", cfg.DNS.NodePrefix, nodeID, cfg.Ports.LocalObjectStore)
     conn, _ := grpc.Dial(otherLocalAddress, grpc.WithInsecure())
-    x := conn.Copy(ctx, &pb.CopyRequest{Uid : resp.Uid, requester : nodeID})
+    c = NewLocalObjStoreClient(conn)
+    x := c.Copy(ctx, &pb.CopyRequest{Uid : resp.Uid, requester : nodeID})
     
     gcsObjClient.NotifyOwns(ctx, &pb.NotifyOwnsRequest{Uid: req.Uid, NodeId: localNodeID})
     localObjectChannels[resp.Uid] = <- x.ObjectBytes
