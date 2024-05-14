958e523057b3949e0742c85bfa33d3fe61d5bdd3
Rodrigo Castellon <rjcaste@stanford.edu>
2024-05-13 21:55:01 -0700

    actually provide name of function to worker.Run()

--- go/cmd/localscheduler/main.go
+++ go/cmd/localscheduler/main.go
@@ -63,7 +63,7 @@ func (s *server) Schedule(ctx context.Context, req *pb.ScheduleRequest) (*pb.Sch
         defer conn.Close()
 
 		workerClient := pb.NewWorkerClient(conn)
-		_, err = workerClient.Run(ctx, &pb.RunRequest{Uid: uid, Args: req.Args, Kwargs: req.Kwargs})
+		_, err = workerClient.Run(ctx, &pb.RunRequest{Uid: uid, Name: req.Name, Args: req.Args, Kwargs: req.Kwargs})
 		if err != nil {
 			log.Fatalf("cannot contact worker %d: %v", worker_id, err)
 		}
